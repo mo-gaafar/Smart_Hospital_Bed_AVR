72e9dc5b65e4dc352d5bf2bd0b94261baf086350
hassan141 <73851990+hassan141@users.noreply.github.com>
2022-01-02 06:02:54 +0200

    adc

---
+++ include/ADC_DRIVER/ADC.c
@@ -0,0 +1,49 @@
+#include"ADC.h"
+#include"STD_TYPES.h"
+#include "avr/io.h"
+#include"BIT_MATH.h"
+
+/* 
+MUX4...0        (+ Differential Input)      ( – Differential Input)      Gain
+
+01001                  ADC1                         ADC0                10x
+
+01011                  ADC1                         ADC0                200x
+
+*/
+
+
+void ADC_Init (void)
+{
+	// ADC Enable
+	SET_BIT(ADCSRA,7);
+
+	// Prescaler Configuration as 128
+	SET_BIT(ADCSRA,2);
+	SET_BIT(ADCSRA,1);
+	SET_BIT(ADCSRA,0);
+
+
+	// Referance Voltage as AVCC with external capacitor
+	CLR_BIT(ADMUX,7);
+	SET_BIT(ADMUX,6);
+
+	// ADC Data Rgith Adjustment
+	CLR_BIT(ADMUX,5);
+}
+
+u16 ADC_Read (u8 channel)
+{
+	// ADC Channel Selection
+	
+	ADMUX &=0b11100000;
+	ADMUX |=(channel &0b00011111);
+
+	// Start Single Convertion
+	SET_BIT(ADCSRA,6);
+
+	//wait for the conversion
+	while (ADCSRA & (1 << 6));
+
+	return ADC;
+}
---
+++ include/ADC_DRIVER/ADC.h
@@ -0,0 +1,8 @@
+
+#ifndef ADC
+#define ADC
+extern void ADC_Init (void); // ADC Initialization And Enable
+extern u16 ADC_Read (u8 channel); // Read From The ADC Channel
+
+
+#endif /* ADC_INITIALIZATION_H_ */
